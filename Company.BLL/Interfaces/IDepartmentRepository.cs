81b8c1484e11ed57c9bb73b1f36401bacb853961
Ahmed <Ahmed@DESKTOP-7U319EG>
2025-03-17 14:36:46 +0200

    Department Module BLL

---
+++ Company.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.DAL.Models;
+using Microsoft.Identity.Client;
+
+namespace Company.BLL.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+        Department? Get(int id);
+
+        int Add(Department model);
+
+        int Update(Department model);
+
+        int Delete(Department model);
+
+    }
+}
