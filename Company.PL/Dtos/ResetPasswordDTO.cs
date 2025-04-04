5ff64400aca993022ca51366d7a77e6179e34db2
Ahmed <Ahmed@DESKTOP-7U319EG>
2025-04-04 19:14:53 +0200

    password

---
+++ Company.PL/Dtos/ResetPasswordDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.PL.Dtos
+{
+    public class ResetPasswordDTO
+    {
+        [Required(ErrorMessage = "Password is Required!")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "ConfirmPassword is Required!")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Password and ConfirmPassword do not match!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ Company.PL/Helpers/EmailSettings.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace Company.PL.Helpers
+{
+    public static class EmailSettings
+    {
+        public static bool SendEmail(Email email)
+        {
+            // Mail Server : Gmail
+            // SMTP Server : smtp.gmail.com
+            // Port : 587
+
+            try
+            {
+                var client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential("ahmedkhattaba221@gmail.com", "gayyscrnrhmghzza");
+                client.Send("ahmedkhattaba221@gmail.com", email.To, email.Subject, email.Body);
+
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
